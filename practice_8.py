20b7d9864351652ed2b4e1ea25d717a6ffde6850
Ibsy Matute <ibsy@rocketmail.com>
2022-10-03 18:27:46 -0500

    completed codewars algorithm

---
+++ practice_8.py
@@ -0,0 +1,20 @@
+# A Narcissistic Number is a positive number which is the sum of its own digits, 
+# each raised to the power of the number of digits in a given base. In this Kata, 
+# we will restrict ourselves to decimal (base 10).
+
+# For example, take 153 (3 digits), which is narcisstic:
+# 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+# and 1652 (4 digits), which isn't:
+# 1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+
+def narcissistic(value):
+
+    power = len(str(value))
+    value_list = [int(v) for v in str(value)]
+    sum_of_power = [x**power for x in value_list]
+
+    if sum(sum_of_power) == value:
+        return True
+    return False
+
+print(narcissistic(1652))
